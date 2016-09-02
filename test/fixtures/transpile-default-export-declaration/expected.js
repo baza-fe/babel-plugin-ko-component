e562670d075bc3057c3499b7de1b72e1a96c3d8b
BinRui Guan <binrui.guan@hirede.com>
2016-09-02 17:48:19 +0800

    Update test cases

--- test/fixtures/transpile-default-export-declaration/expected.js
+++ test/fixtures/transpile-default-export-declaration/expected.js
@@ -5,5 +5,5 @@ const __ko_component__ = {};
 __ko_component__.name = __ko_component__.name || __ko_component_label__;
 __ko_component__.style = __ko_component__.style || __ko_component_style__;
 __ko_component__.template = __ko_component__.template || __ko_component_template__;
-ko.components.register(__ko_component__.name, __ko_component__);
+ko.components.register(__ko_component__);
 export default __ko_component__;
